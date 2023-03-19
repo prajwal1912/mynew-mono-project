887c7ccda7f573809c37a99b0664dd9357007bd6
prajwal isane <118800143+prajwal1912@users.noreply.github.com>
2023-03-19 16:16:34 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIA5WI7R4JFNKMUSIFC"
-secret_key = "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"
-}
+access_key = "AKIA5WI7R4JFMJ64476K"
+secret_key = "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"
